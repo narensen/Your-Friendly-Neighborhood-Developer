aaf0f5eb41cbfeb94c1cb6adeab5ccd726bb546a
narensen <scartegy@gmail.com>
2025-03-20 21:37:08 +0530

    we fixed it

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
-from config import *
-from models import *
+from api.config import *
+from api.models import *
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
